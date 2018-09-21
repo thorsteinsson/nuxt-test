734e5a9f268eb83df466472efc991332d07191f6
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2018-09-21 21:27:47 +0000

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="red">Red</h1>
+    <Green>Green</Green>
+  </div>
+</template>
+
+<script>
+import styled from 'vue-styled-components'
+
+const Green = styled.h1`
+  color: green;
+`
+
+export default {
+  components: {
+    Green
+  }
+}
+</script>
+
+<style>
+.red
+{
+  color: red;
+}
+</style>
